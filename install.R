9c47d7e58bf5f86a64df27984e65d1b2cf0a5924
gyeb-kodie <gyebkodie@gmail.com>
2025-09-02 15:53:59 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
-install.packages("readr")
-options(repos = c(CRAN = "https://cran.r-project.org"))
-install.packages(c("readr","ggplot2","tidyr"))
-options(repos = c(CRAN = "https://cran.r-project.org"))
-install.packages(c("readr","ggplot2","tidyr","shiny"))
+install.packages("tidyverse", dependencies = TRUE)
+install.packages("rmarkdown", dependencies = TRUE)
+install.packages("IRkernel", dependencies = TRUE)
+
+IRkernel::installspec(user = FALSE)
