6bb5cd19899157aa190181fe6320cf626ecc13e8
gyeb-kodie <gyebkodie@gmail.com>
2025-09-16 13:49:19 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,7 @@
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
+install.packages("shiny"), dependencies = TRUE)
+install.packages("skimr"), dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
